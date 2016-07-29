836fea27daec8bdcc50d1d7bebbdee9a000a2466
Eric Chiu <c3366301@gmail.com>
2016-07-29 22:23:30 +0800

    add product scaffold

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -13,15 +13,15 @@ class Cart
   def empty?
     @items.empty?
   end
+
 end
 
 RSpec.describe Cart, type: :model do
-  describe "購物車相關功能" do
-    it "可以加到購物車" do
+  describe '購物車相關功能' do
+    it '可以加到購物車' do
       cart = Cart.new
-      cart.add_item(1)
+      cart.add_item(2)
 
-      # expect(cart.empty?).to be false
       expect(cart.empty?).to be false
     end
   end
