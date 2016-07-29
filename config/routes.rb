359b8c6c47d8587bc84f781762293faa6c4344b0
Eric Chiu <c3366301@gmail.com>
2016-07-29 22:02:43 +0800

    add product scaffold and  model cart with test

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :products
   root "pages#index"
+
+
 end
---
+++ spec/models/cart_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+class Cart
+
+  def initialize
+    @items = []
+  end
+
+  def add_item(product)
+    @items << product
+  end
+
+  def empty?
+    @items.empty?
+  end
+end
+
+RSpec.describe Cart, type: :model do
+  describe "購物車相關功能" do
+    it "可以加到購物車" do
+      cart = Cart.new
+      cart.add_item(1)
+
+      # expect(cart.empty?).to be false
+      expect(cart.empty?).to be false
+    end
+  end
+end
