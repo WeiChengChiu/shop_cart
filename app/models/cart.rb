bb339ac606309922f80ae732284a0cedec92b343
Eric Chiu <c3366301@gmail.com>
2016-07-29 22:31:46 +0800

    rspec-cart move to model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,16 @@
-class Cart < ApplicationRecord
+class Cart
+
+  def initialize
+    @items = []
+  end
+
+  def add_item(product)
+    @items << product
+  end
+
+  def empty?
+    @items.empty?
+  end
+
 end
+
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,27 +1,13 @@
 require 'rails_helper'
 
-class Cart
 
-  def initialize
-    @items = []
-  end
-
-  def add_item(product)
-    @items << product
-  end
-
-  def empty?
-    @items.empty?
-  end
-
-end
 
 RSpec.describe Cart, type: :model do
   describe '購物車相關功能' do
     it '可以加到購物車' do
       cart = Cart.new
-      cart.add_item(2)
 
+      cart.add_item(2)
       expect(cart.empty?).to be false
     end
   end
