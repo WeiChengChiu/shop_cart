e5a054b6b15fe55f511817a12db1fbca603cf136
Eric Chiu <c3366301@gmail.com>
2016-07-30 12:17:55 +0800

    refactoring

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,17 +1,3 @@
-class CartItem
-  attr_reader :product_id, :quantity
-
-  def initialize(product_id, quantity = 1 )
-    @product_id = product_id
-    @quantity = quantity
-  end
-
-  def increment
-    @quantity += 1
-  end
-
-end
-
 class Cart
 
   attr_reader :items
@@ -22,18 +8,18 @@ class Cart
 
   def add_item(product_id)
 
-    item = @items.find { |x| x.product_id == product_id }
+    item = items.find { |x| x.product_id == product_id }
 
     if item
       item.increment
     else
-      @items << CartItem.new(product_id)
+      items << CartItem.new(product_id)
     end
 
   end
 
   def empty?
-    @items.empty?
+    items.empty?
   end
 
 end
